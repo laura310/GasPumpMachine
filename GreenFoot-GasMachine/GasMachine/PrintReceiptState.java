f15ce3237dc216cdcf405d471da0690a639e3267
Laura Zhou <laurajoe310@yahoo.com>
2017-04-02 16:53:29 -0700

    Added constructor and print() in PrintReceiptState.

--- GreenFoot-GasMachine/GasMachine/PrintReceiptState.java
+++ GreenFoot-GasMachine/GasMachine/PrintReceiptState.java
@@ -1,3 +1,27 @@
-public class PrintReceiptState {
+//this assumes that there's no State interface to be implemented.
 
+/**
+ * PrintReceiptState is to print the receipt if:
+ * Car Wash option is "Yes" or
+ * Car Wash option is "No" and Print Receipt option is "Yes".
+ *
+ *
+ */
+
+public class PrintReceiptState {
+	final String costMsg = "Your total cost is: ";
+	final String thankyouMsg = "Thank you!";
+
+	GasPumpMachine gasPumpMachine;
+	int cost = 0;
+
+	public PrintReceiptState(GasPumpMachine gasPumpMachine, int cost) {
+		this.gasPumpMachine = gasPumpMachine;
+		this.cost = cost;
+	}
+
+	public void print() {
+		System.out.println(costMsg + cost);
+		System.out.println(thankyouMsg);
+	}
 }
